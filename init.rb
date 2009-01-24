7c3d74c67833a24e6c2d696bebaa90a2308163a3
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-23 20:37:59 -0800

    Adding generated plugin

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_mobile do
+  name 'Mobile plugin'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/show/redmine-mobile'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "A Redmine plugin to provide simpler views for mobile users"
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+end
